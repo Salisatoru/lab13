4b7f047311325c9d0d813e3840385c3f2b80d7bc
Salisatoru <95071118+Salisatoru@users.noreply.github.com>
2021-12-24 16:11:54 +0700

    lab13_1.cpp.completed

---
+++ lab13_1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include <ctime>
+using namespace std;
+
+long long int fibonacci(long long int x);
+
+int main(){
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
+}
+
+long long int fibonacci(long long int x)
+{
+    if (x==1) return 1 ;
+    if (x==0) return 0 ;
+    else return fibonacci(x-1) + fibonacci(x-2) ;
+}
