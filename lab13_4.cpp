3a7e30afd0f9dd2af792dc14a66243a655b0fa2b
Salisatoru <95071118+Salisatoru@users.noreply.github.com>
2021-12-24 15:57:45 +0700

    lab13_4.cpp.completed

---
+++ lab13_4.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+template <typename type> type mySwap ( type & ,type & );
+
+int main(){
+	int x, y;
+	string a, b;
+	char p, q;
+	
+	cin >> x >> y >> a >> b >> p >> q;
+	
+	cout << "Before swapping:\n";
+	cout << "x = " << x << ", y = " << y << "\n";
+	mySwap(x,y);
+	cout << "After swapping:\n";
+	cout << "x = " << x << ", y = " << y << "\n";
+	
+	cout << "Before swapping:\n";
+	cout << "a = " << a << ", b = " << b << "\n";
+	mySwap(a,b);
+	cout << "After swapping:\n";
+	cout << "a = " << a << ", b = " << b << "\n";
+	
+	cout << "Before swapping:\n";
+	cout << "p = " << p << ", q = " << q << "\n";
+	mySwap(p,q);
+	cout << "After swapping:\n";
+	cout << "p = " << p << ", q = " << q << "\n";
+
+	return 0;
+}
+
+template <typename type> type mySwap( type &A , type &B )
+{
+	type Q=A;
+	A=B;
+	B=Q;
+	return A;
+}
